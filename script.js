22d461d8787150418831c9f74c560775be4b026a
Abhinav Sharma <vats.abhinav247@gmail.com>
2026-01-21 22:54:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+const toggleBtn = document.getElementById("themeToggle");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+}
+
+toggleBtn.addEventListener("click", () => {
+    let theme = document.documentElement.getAttribute("data-theme");
+
+    if (theme === "dark") {
+        document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
+    } else {
+        document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
+    }
+});
